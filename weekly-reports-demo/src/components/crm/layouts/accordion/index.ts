71c6bfa93147df2026340904bb3498a820e9dadc
jstncg <justin04cheng@gmail.com>
2026-02-03 01:15:52 -0500

    refactor(crm): delete unused layouts and components

--- weekly-reports-demo/src/components/crm/layouts/accordion/index.ts
+++
@@ -1,3 +0,0 @@
-export { AccordionLayout } from "./accordion-layout";
-export { ContactListItem } from "./contact-list-item";
-export { ContactExpandedContent } from "./contact-expanded-content";
--- weekly-reports-demo/src/components/crm/layouts/master-detail/index.ts
+++
@@ -1,3 +0,0 @@
-export { MasterDetailLayout } from "./master-detail-layout";
-export { ContactSidebar } from "./contact-sidebar";
-export { ContactDetailPane } from "./contact-detail-pane";
